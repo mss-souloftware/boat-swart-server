af7413126c9deebdb5272cc5d7d1a7dc4f654498
jawad sandbox <jawadsandboxemail@gmail.com>
2024-06-13 13:59:33 +0500

    try: fix

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ app.use(express.json({ limit: '100mb' }))
 app.use(cors())
 app.use(reqResInspector())
 
+app.use('/test', (req: any, res: any) => res.send('hi'))
 app.use('/api/v1', apiRoutes)
 
 export default app
