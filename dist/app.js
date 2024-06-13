9870576ceef1d3d03cd2956e2ad814e37dabe51f
jawad sandbox <jawadsandboxemail@gmail.com>
2024-06-13 14:02:45 +0500

    try: fix

--- dist/app.js
+++ dist/app.js
@@ -12,6 +12,6 @@ app.use(express_1.default.static('uploads'));
 app.use(express_1.default.json({ limit: '100mb' }));
 app.use((0, cors_1.default)());
 app.use((0, express_req_res_inspector_1.default)());
-app.use('/test', (req, res) => res.send('hi'));
+app.get('/test', (req, res) => res.send('hi'));
 app.use('/api/v1', index_1.default);
 exports.default = app;
